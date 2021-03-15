7ca19a238861f09a5d3c9a318fe386a8c195132c
Kai Zhu <zhuk2@mcmaster.ca>
2021-03-15 18:48:20 -0400

    Added option to unanchor IK chain

--- IKgui.py
+++ IKgui.py
@@ -6,7 +6,7 @@ from tkinter import *
 import math
 from lib.ikchain import IKChain
 
-VERSION = "0.6"
+VERSION = "0.8"
 
 master = Tk()
 master.title("IK GUI  " + VERSION)
@@ -15,6 +15,8 @@ master.configure(bg="black")
 
 l1text = StringVar()
 txtLen = "100,100,100"
+
+fixed = BooleanVar()
 
 CANVAS_W = 1000
 CANVAS_H = 750
@@ -29,7 +31,7 @@ def canvas_drag(event):
 
     c.coords(tgt, x-10, y-10, x+10, y+10)   # move the blue target pos dot
 
-    arms.solve([x, y], fixed = True)                      # solve and render IK
+    arms.solve([x, y], fixed = fixed.get())                      # solve and render IK
     arms.update()
 
     labeltext = ""                          # update status bar
@@ -69,12 +71,17 @@ c = Canvas(master, width = CANVAS_W, height = CANVAS_H, bg="black", highlightthi
 c.grid(row=1, columnspan=10)
 
 e_lengths = Entry(master, bg="black", fg="white", justify="left", insertbackground  = "gray",relief = FLAT)
-e_lengths.grid(row=2, column=0, sticky = W+E, columnspan=10)
+e_lengths.grid(row=2, column=0, sticky = W+E, columnspan=8)
 e_lengths.insert(0, txtLen)
 e_lengths.bind("<Return>", newIK)
 
 l1 = Label(master,text="",bg = "black", fg="white", textvariable=l1text)
 l1.grid(row=3, column=0, sticky = W+E, columnspan=10)
+
+c_pattern = Checkbutton(master, text="Anchored", bg="black", fg ="white", activebackground = "black", activeforeground = "gray", selectcolor="black", variable=fixed, relief = FLAT, borderwidth=0)
+fixed.set(True)
+c_pattern.grid(row=2, column=8, sticky = E)
+#c_pattern.insert(0, str(maze_h))
 
 b_exit = Button(master,text="Exit", fg="white", bg = "black",relief=FLAT, width = 10)
 b_exit.grid(row=2, column=9, sticky=E)
